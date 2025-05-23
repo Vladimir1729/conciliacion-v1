e1e9581a6dcc8ec6b390a72cfbc4c58dd7eb3e71
Vladimir <vgarcia@quom.com.mx>
2025-05-23 10:32:33 -0600

    conciliacion v3

--- main_conciliador.py
+++ main_conciliador.py
@@ -1,9 +1,14 @@
+import os
 from airflow.decorators import dag
 from datetime import datetime
 from slack_alerts import start_alert, success_alert, failure_alert
 from comparador_tasks import comparar_referencias
 
-CONFIG_PATH = ''
+
+#Ruta del directorio donde están los JSON
+#CONFIG_PATH = ''
+CONFIG_DIR = os.path.join(os.path.dirname(__file__), 'configs')
+
 
 @dag(
     dag_id='comparar_referencias_dag',
@@ -14,11 +19,29 @@ CONFIG_PATH = ''
 )
 def comparar_dag():
     start = start_alert()
+    all_results = []
+
+
+    #iteramos sobre todos los archivos (.json) del directorio
+    for file in os.listdir(CONFIG_DIR):
+        if file.endswith('.json'):
+            config_path = os.path.join(CONFIG_DIR, file)
+            result = comparar_referencias.override(task_id = f"comparar_{file.replace('.json', '')}")(config_path = config_path)
+            start >> result
+            all_results.append(result)
+
+            #Agregamos alertas individuales por cada comparación si deseas
+            result >> success_alert.override(task_id = f"success_alert_{file.replace('.json', '')}")(result)
+            result >> failure_alert.override(task_id = f"failure_alert_{file.replace('.json', '')}")()
+
+
+    '''
     result = comparar_referencias(config_path=CONFIG_PATH)
     success = success_alert(result)
     fail = failure_alert()
 
     start >> result
     result >> [success, fail]
+    '''
 
 dag = comparar_dag()
